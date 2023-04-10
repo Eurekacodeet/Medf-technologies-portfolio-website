f27f471578cab78a88b243cae296d9242bc5a8f6
kirubel <kirato747@gmail.com>
2023-04-10 19:50:46 +0300

    about page done

--- src/Components/Aboutus/Aboutus.js
+++ src/Components/Aboutus/Aboutus.js
@@ -2,30 +2,146 @@ import React from 'react'
 
 const Aboutus = () => {
   return (
-    <div className='bg-black p-10 flex flex-col items-center h-screen '>
+    <div className='bg-black p-10 flex flex-col items-center h-full '>
       <div >
       <h1 className='text-[1.6rem] font-bold break-words mr-4 text-white whitespace-pre '>M e e t    t h e   t e a m </h1>
 
 <div className='rounded-full w-10 h-[0.4rem] 
 mt-4 hover:w-[16.7rem] transition-width bg-white duration-500 ease-in-out shadow shadow-white'></div>
       </div> 
-    <div className=' flex flex-wrap bg-white mt-10 p-10 rounded'>
-      <div class="grid grid-rows-3 grid-flow-col gap-4">
-      <div class="row-span-2 col-span-2 ...">
-        <p className='text-base '>Hello, I'm</p>
-      <h2 className='text-3xl font-bold pt-2'>Kirubel</h2>
-      <p className='text-base pt-2'>`Web developer with 2 years of experience akwjdhkawdilawdojaidhakfAHBFahfiafafawfa`</p>
-      
-      </div>
 
-  <div class="row-span-3 ..."><img src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681059700/qqaoi6ju1nwaaglmi9vv.png"/></div>
+
+    <div className=' flex flex-row justify-evenly bg-white mt-10 p-10 rounded'>
+
+  <div class=" w-2/6 mr-4">
+    <p class='text-base max-w-md'>Hello, I'm</p>
+    <h2 class='text-3xl font-bold pt-2'>Kirubel</h2>
+    <p class='text-base pt-2 max-w-md break-words'>Web developer with 2 years of experience akwjdhkawdilawdojaidhakfAHBFahfiafafawfa</p>
+  </div>
+
+
+
+  <div class="row-span-3 w-2/5 mr-4"><img src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681139440/hmgglatf5qjig2tqvjom.png"/></div>
  
  
-  <div>
-
+  <div className='flex flex-col w-2/5'>
+  <div className='flex justify-center items-center'>
+  <div class="grid grid-cols-3 gap-4">
+  <div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] m-auto flex justify-center items-center rounded">
+    <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
+  </div>
+  <div class="col-span-1"></div>
+  <div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] flex justify-center items-center rounded">
+    <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
+  </div>
+  <div class="col-span-1"></div>
+  <div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] flex justify-center items-center rounded">
+    <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
   </div>
 </div>
-    </div></div>
+
+  </div>
+  <div className=''>
+    <h1 className='text-3xl font-bold pt-2'>Selected works</h1>
+    <ul className='text-base pt-2'>
+      <li>Link 1</li>
+      <li>Link 2</li>
+    </ul>
+  </div>
+</div>
+
+
+
+    </div>
+
+    <div className=' flex flex-row justify-evenly bg-white mt-10 p-10 rounded'>
+
+<div class=" w-2/6 mr-4">
+  <p class='text-base max-w-md'>Hello, I'm</p>
+  <h2 class='text-3xl font-bold pt-2'>Abubeker</h2>
+  <p class='text-base pt-2 max-w-md break-words'>Web developer with 2 years of experience akwjdhkawdilawdojaidhakfAHBFahfiafafawfa</p>
+</div>
+
+
+
+<div class="row-span-3 w-2/5 mr-4"><img src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681145174/bmh2e4qmekkxaohdkfqh.png"/></div>
+
+
+<div className='flex flex-col w-2/5'>
+<div className='flex justify-center items-center'>
+<div class="grid grid-cols-3 gap-4">
+<div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] m-auto flex justify-center items-center rounded">
+  <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
+</div>
+<div class="col-span-1"></div>
+<div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] flex justify-center items-center rounded">
+  <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
+</div>
+<div class="col-span-1"></div>
+<div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] flex justify-center items-center rounded">
+  <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
+</div>
+</div>
+
+</div>
+<div className=''>
+  <h1 className='text-3xl font-bold pt-2'>Selected works</h1>
+  <ul className='text-base pt-2'>
+    <li>Link 1</li>
+    <li>Link 2</li>
+  </ul>
+</div>
+</div>
+
+
+
+  </div>
+  <div className=' flex flex-row justify-evenly bg-white mt-10 p-10 rounded'>
+
+<div class=" w-2/6 mr-4">
+  <p class='text-base max-w-md'>Hello, I'm</p>
+  <h2 class='text-3xl font-bold pt-2'>Abel</h2>
+  <p class='text-base pt-2 max-w-md break-words'>Web developer with 2 years of experience akwjdhkawdilawdojaidhakfAHBFahfiafafawfa</p>
+</div>
+
+
+
+<div class="row-span-3 w-2/5 mr-4"><img src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681059700/qqaoi6ju1nwaaglmi9vv.png"/></div>
+
+
+<div className='flex flex-col w-2/5'>
+<div className='flex justify-center items-center'>
+<div class="grid grid-cols-3 gap-4">
+<div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] m-auto flex justify-center items-center rounded">
+  <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
+</div>
+<div class="col-span-1"></div>
+<div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] flex justify-center items-center rounded">
+  <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
+</div>
+<div class="col-span-1"></div>
+<div class="col-span-1 bg-zinc-500 w-[9vw] h-[9vw] flex justify-center items-center rounded">
+  <img className="w-full h-full object-contain rounded" src="https://res.cloudinary.com/dvqawl4nw/image/upload/v1681141804/e0i9e4rmkdwekwzh0yr7.png" />
+</div>
+</div>
+
+</div>
+<div className=''>
+  <h1 className='text-3xl font-bold pt-2'>Selected works</h1>
+  <ul className='text-base pt-2'>
+    <li>Link 1</li>
+    <li>Link 2</li>
+  </ul>
+</div>
+</div>
+
+
+
+  </div>
+    
+    
+    
+    </div>
   )
 }
 
