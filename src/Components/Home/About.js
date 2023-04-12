43388e6948dd3ea1d575d09bbcd92f55768072c5
kirubel <kirato747@gmail.com>
2023-04-12 13:48:47 +0300

    hover effect fixes

--- src/Components/Home/About.js
+++ src/Components/Home/About.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='flex flex-col bg-black p-10 text-white'>
+    <div className=' bg-black p-10 text-white'>
         
-        
-       <h1 className='text-3xl py-4 font-bold'>About</h1> 
-       <div className='rounded-full bg-white w-10 h-[0.4rem] 
-mt-2 mb-4 hover:w-24 transition-width duration-500 ease-in-out shadow shadow-white'></div>
+        <div className='relative group'>
+  <h1 className='text-3xl py-4 font-bold hover:text-white mb-8 hover:cursor-default'>About</h1>
+  <div className='rounded-full bg-white w-10 h-[0.4rem] mt-2 
+               absolute left-0 bottom-0 transition-all duration-500 ease-in-out shadow
+                shadow-white group-hover:w-24'></div>
+</div>
+
+     
 
         <p id="p_about">Analyse datasets in order to draw conclusions about information with specific system 
             Analyse datasets in order to draw conclusions about information with specific system Analyse
