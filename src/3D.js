4a7c98c1caa5f56c5b467a5361764f2c622b4af1
Abubeker Shamil <ababeker.k@gmail.com>
2023-04-21 11:06:37 +0300

    Minor Improvements

--- src/3D.js
+++ src/3D.js
@@ -7,14 +7,14 @@ const Dh = () => {
   return (
     <div className='flex flex-row bg-black text-white p-10 h-[80vh]'>
       <div className='w-2/5 mr-10 '>
-        <p className='text-sm'>Obtain High Quality Webapps</p>
+        <p className='text-sm'>Quality Services, Robust Speed and Flexible Delivery</p>
         <div className='relative group'>
           <h1 className='text-3xl py-4 font-bold hover:text-white mb-8 hover:cursor-default'>Full Stack Web Development</h1>
           <div className='rounded-full bg-white w-10 h-[0.15rem] mt-1
                absolute left-0 bottom-0 transition-all duration-500 ease-in-out shadow
                 shadow-white group-hover:w-[87%]'></div>
         </div>
-        <p className='text-sm'>We help you to grow you business by developing awesome and interactive web apps </p>
+        <p className='text-sm'>We help you to grow your business by developing awesome and interactive web and mobile apps </p>
       </div>
       <div className='w-3/5 '>
         <Canvas className=''>
